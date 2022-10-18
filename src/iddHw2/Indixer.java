f06989d30a46d15c51fb0c85b4676d3343e00016
MarcelloPonzi <marcelloponzi@gmail.com>
2022-10-18 12:59:19 +0200

    Commit iniziale

--- src/iddHw2/Indixer.java
+++ src/iddHw2/Indixer.java
@@ -1,81 +1,85 @@
 package iddHw2;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.CharArraySet;
-import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
-import org.apache.lucene.analysis.miscellaneous.PerFieldAnalyzerWrapper;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.codecs.Codec;
-import org.apache.lucene.codecs.simpletext.SimpleTextCodec;
+import org.apache.lucene.analysis.core.LowerCaseFilterFactory;
+import org.apache.lucene.analysis.core.WhitespaceTokenizerFactory;
+import org.apache.lucene.analysis.custom.CustomAnalyzer;
+import org.apache.lucene.analysis.it.ItalianAnalyzer;
+import org.apache.lucene.analysis.miscellaneous.WordDelimiterGraphFilterFactory;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.FieldInfos;
-import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
+
 public class Indixer {
-	Path pathNome = Paths.get("target/idx0");
-	Path pathContenuto = Paths.get("target/idx1");
-	
-	public void indicizzaNome() throws IOException {
-		try (Directory directory = FSDirectory.open(pathNome)) {
-            indexDocs(directory, new SimpleTextCodec());
-            try (IndexReader reader = DirectoryReader.open(directory)) {
-                IndexSearcher searcher = new IndexSearcher(reader);
-                Collection<String> indexedFields = FieldInfos.getIndexedFields(reader);
-                for (String field : indexedFields) {
-                    System.out.println(searcher.collectionStatistics(field));
-                }
-            } finally {
-                directory.close();
-            }
+	Path path = Paths.get("target/idx"); 
 
-        }
-    }
-	
-    private void indexDocs(Directory directory, Codec codec) throws IOException {
-        Analyzer defaultAnalyzer = new StandardAnalyzer();
-        CharArraySet stopWords = new CharArraySet(Arrays.asList("in", "dei", "di"), true);
-        Map<String, Analyzer> perFieldAnalyzers = new HashMap<>();
-        perFieldAnalyzers.put("contenuto", new StandardAnalyzer(stopWords));
-        perFieldAnalyzers.put("titolo", new WhitespaceAnalyzer());
+	public void indicizza() throws IOException {
+		Directory directory = FSDirectory.open(path);
+		IndexWriter writer = new IndexWriter(directory, new IndexWriterConfig());
+		writer.deleteAll();
+        File dirfiles = new File("Resources");
+        File[] files = dirfiles.listFiles();
+        Analyzer analnome = CustomAnalyzer.builder()
+        		 			.withTokenizer(WhitespaceTokenizerFactory.class)
+        		 			.addTokenFilter(LowerCaseFilterFactory.class)
+        		 			.addTokenFilter(WordDelimiterGraphFilterFactory.class)
+        		 			.build();
+        
+        Analyzer analcontenuto = CustomAnalyzer.builder()
+					 			.withTokenizer(WhitespaceTokenizerFactory.class)
+					 			.addTokenFilter(LowerCaseFilterFactory.class)
+					 			.addTokenFilter(WordDelimiterGraphFilterFactory.class)
+					 			.build();
 
-        Analyzer analyzer = new PerFieldAnalyzerWrapper(defaultAnalyzer, perFieldAnalyzers);
-        IndexWriterConfig config = new IndexWriterConfig(analyzer);
-        if (codec != null) {
-            config.setCodec(codec);
-        }
-        IndexWriter writer = new IndexWriter(directory, config);
-        writer.deleteAll();
 
-        Document doc1 = new Document();
-        doc1.add(new TextField("titolo", "Come diventare un ingegnere dei dati, Data Engineer?", Field.Store.YES));
-        doc1.add(new TextField("contenuto", "Sembra che oggigiorno tutti vogliano diventare un Data Scientist  ...", Field.Store.YES));
-        doc1.add(new StringField("data", "12 ottobre 2016", Field.Store.YES));
+		// Fetching all the files
+		for (File file : files) {
+			if(file.isFile()) {
+				BufferedReader inputStream = null;
+				String contenuto;
+				String nome;
+				Document doc;
+				try {
+					inputStream = new BufferedReader(new FileReader(file));
+					while ((contenuto = inputStream.readLine()) != null && (nome=file.getName()) != null) {	
+						nome=nome.replace(".txt", "");
+						//indicizza qui contenuto e nome
+						doc = new Document();
+						doc.add(new TextField("nome", nome, Field.Store.YES));
+						doc.add(new TextField("contenuto", contenuto, Field.Store.YES));
+						writer.addDocument(doc);
+					}
+				}catch(IOException e) {
+					System.out.println(e);
+				}
+				finally {
+					if (inputStream != null) {
+						inputStream.close();
+					}
+				}
+			}
+		}
+		writer.commit();
+		writer.close();
 
-        Document doc2 = new Document();
-        doc2.add(new TextField("titolo", "Curriculum Ingegneria dei Dati - Sezione di Informatica e Automazione", Field.Store.YES));
-        doc2.add(new TextField("contenuto", "Curriculum. Ingegneria dei Dati. Laurea Magistrale in Ingegneria Informatica ...", Field.Store.YES));
+	}
 
-        writer.addDocument(doc1);
-        writer.addDocument(doc2);
 
-        writer.commit();
-        writer.close();
-    }
+
+
+
+
 }
--- src/iddHw2/hw2.java
+++ src/iddHw2/hw2.java
@@ -1,10 +1,18 @@
 package iddHw2;
 
+import java.io.IOException;
+
 public class hw2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Indixer i = new Indixer();
+		try {
+			i.indicizza();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
